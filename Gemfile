512e6f6b74fa450773adbecaa22188e6e49dd258
Joe Heth <joeheth@gmail.com>
2014-02-12 18:05:24 -0800

    Adding dependencies on savon and curb. Implemented SOAP client authentication and who_am_i action.

--- Gemfile
+++ Gemfile
@@ -1,4 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'savon', '~> 2.0'
+gem 'curb'
+
 # Specify your gem's dependencies in mscrm-soap.gemspec
 gemspec
--- lib/mscrm/soap.rb
+++ lib/mscrm/soap.rb
@@ -1,7 +1,16 @@
 require "mscrm/soap/version"
+require "mscrm/authentication"
 
 module Mscrm
   module Soap
-    # Your code goes here...
+
+	class Client
+		attr_accessor :client
+
+		def initialize(organization_name)
+			@client = Savon.client(wsdl: "https://#{organization_name}.api.crm.dynamics.com/XRMServices/2011/Organization.svc?wsdl=wsdl0")
+		end
+
+	end
   end
 end
