c214be421b611cb687f5821c1bd55f4e852589e7
Joe Heth <joeheth@gmail.com>
2014-06-17 21:55:53 -0700

    Adds camel_case option to EntityReference#to_xml to fix associate/disassociate requests. fixes #3

--- spec/lib/xml/entity_reference_spec.rb
+++ spec/lib/xml/entity_reference_spec.rb
@@ -14,7 +14,7 @@ describe DynamicsCRM::XML::EntityReference do
     end
 
     context "#to_xml" do
-      it {
+      it "outputs entityReference with no namespace" do
         # Spacing here is intentional to match created string.
         expected_xml = %Q{
         <entityReference>
@@ -24,7 +24,20 @@ describe DynamicsCRM::XML::EntityReference do
         </entityReference>
         }
         subject.to_xml.should == expected_xml
-      }
+      end
+
+      it "outputs EntityReference with namespace" do
+        # Spacing here is intentional to match created string.
+        expected_xml = %Q{
+        <b:EntityReference>
+          <b:Id>9BF1325C-A592-E311-B7F3-6C3BE5A8A0C8</b:Id>
+          <b:LogicalName>opportunity</b:LogicalName>
+          <b:Name #{@name ? '' : 'nil="true"'}>#{@name}</b:Name>
+        </b:EntityReference>
+        }
+        subject.to_xml(namespace: 'b', camel_case: true).should == expected_xml
+      end
+
     end
 
     context "#from_xml" do
