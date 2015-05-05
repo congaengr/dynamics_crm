59ec5219f4c17cb1b286fa6b60877fd6cfaf62cc
Carlos André <carlos.ulrich@gmail.com>
2015-05-05 19:31:53 +0100

    Add money attribute

---
+++ spec/lib/xml/money_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe DynamicsCRM::XML::Money do
+
+  describe '#initialize' do
+    let(:value) { 250.0 }
+    let(:precision) { 2 }
+    subject { DynamicsCRM::XML::Money.new(value, precision) }
+
+    context "with a string" do
+      let(:value) { "250.00" }
+      it { subject.to_xml.should eq("250.00") }
+
+      context "and precision is 1" do
+        let(:precision) { 1 }
+        it { subject.to_xml.should eq("250.0") }
+      end
+    end
+
+    context "with a number" do
+      let(:value) { 250 }
+      it { subject.to_xml.should eq("250.00") }
+
+      context "and precision is 1" do
+        let(:precision) { 1 }
+        it { subject.to_xml.should eq("250.0") }
+      end
+    end
+
+    context "with a float" do
+      let(:value) { 250.00 }
+      it { subject.to_xml.should eq("250.00") }
+
+      context "and precision is 1" do
+        let(:precision) { 1 }
+        it { subject.to_xml.should eq("250.0") }
+      end
+    end
+
+  end
+end
