b14682100e0852471b980a145cacbe676c494d0a
Jonathan Atkinson <jonathan.atkinson@introhive.com>
2015-09-14 15:11:32 -0300

    Certain plugin responses come back as JSON, so removing XML parsing and just returning result

--- lib/dynamics_crm/response/raw_result.rb
+++ lib/dynamics_crm/response/raw_result.rb
@@ -3,15 +3,9 @@ module DynamicsCRM
 
     class RawResult
 
-      attr_reader :document
       attr_reader :result_response
 
 	  def initialize(xml)
-	    @document = REXML::Document.new(xml)
-
-	    fault_xml = @document.get_elements("//[local-name() = 'Fault']")
-	    raise XML::Fault.new(fault_xml) if fault_xml.any?
-
 	    @result_response = xml
 	  end
 	end
